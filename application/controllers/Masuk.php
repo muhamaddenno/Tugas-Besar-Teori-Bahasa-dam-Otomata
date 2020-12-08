5a38665d9f9f75ba62b02e492e2864a5eee27cbb
Mercyano <mercyanodandi@gmail.com>
2020-12-08 18:23:19 +0700

    Update Masuk.php

--- application/controllers/Masuk.php
+++ application/controllers/Masuk.php
@@ -26,45 +26,17 @@ class Masuk extends CI_Controller {
 		$id2 = $user->id;
 		$matkul = $this->db->get_where('matkul', ['id' => $id])->row();
 		$ampun = $this->db->get_where('ambil', ['id_matkul' => $id])->row();
-		$pilihan = $this->db->get_where('ambil', ['nama' => "Matematika Diskrit", 'id_mahasiswa' => $id2])->row();
-		$pilihan1 = $this->db->get_where('ambil', ['nama' => "Algoritma dan Pemograman", 'id_mahasiswa' => $id2])->row();
-		$pilihan2 = $this->db->get_where('ambil', ['nama' => "Teori Bahasa dan Automata", 'id_mahasiswa' => $id2])->row();
-		$pilihan3 = $this->db->get_where('ambil', ['nama' => "Sistem Digital", 'id_mahasiswa' => $id2])->row();
-		$pilihan4 = $this->db->get_where('ambil', ['nama' => "Kriptografi", 'id_mahasiswa' => $id2])->row();
-		$pilihan5 = $this->db->get_where('ambil', ['nama' => "Organisasi dan Arsitektur Komputer", 'id_mahasiswa' => $id2])->row();
-		if($matkul->nama == "Algoritma dan Pemograman"  && empty($pilihan)) 
+		$pilihan = $this->db->get_where('ambil', ['nama' => "Kalkulus I", 'id_mahasiswa' => $id2])->row();
+		$pilihan1 = $this->db->get_where('ambil', ['nama' => "Kalkulus II", 'id_mahasiswa' => $id2])->row();
+		
+		if($matkul->nama == "Kalkulus II"  && empty($pilihan)) 
 		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Matematika Diskrit terlebih dahulu!</div>');
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Kalkulus I terlebih dahulu!</div>');
 			redirect('masuk/index');
 		}
-		else if($matkul->nama == "Sistem Digital" && empty($pilihan))
+		else if($matkul->nama == "Kalkulus Lanjut" && empty($pilihan1))
 		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Matematika Diskrit terlebih dahulu!</div>');
-			redirect('masuk/index');
-		}
-		else if($matkul->nama == "Teori Bahasa dan Automata" && empty($pilihan1))
-		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Algoritma dan Pemrograman terlebih dahulu!</div>');
-			redirect('masuk/index');
-		}
-		else if($matkul->nama == "Kriptografi" && empty($pilihan2))
-		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Teori Bahasa dan Automata terlebih dahulu!</div>');
-			redirect('masuk/index');
-		}
-		else if($matkul->nama == "Organisasi dan Arsitektur Komputer" && empty($pilihan3))
-		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Sistem Digital terlebih dahulu!</div>');
-			redirect('masuk/index');
-		}
-		else if($matkul->nama == "Keamanan Jaringan" && empty($pilihan4))
-		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Kriptografi atau  Organisasi dan Arsitektur Komputer terlebih dahulu!</div>');
-			redirect('masuk/index');
-		}
-		else if($matkul->nama == "Keamanan Jaringan" && empty($pilihan5))
-		{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Kriptografi atau  Organisasi dan Arsitektur Komputer terlebih dahulu!</div>');
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Harus mengambil matakuliah Kalkulus II terlebih dahulu!</div>');
 			redirect('masuk/index');
 		}
 		else if(!empty($ampun))
